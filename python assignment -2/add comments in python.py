8f34524c49e552ec7ec63fbc973788788245038e
gaurav kumar patel <93699044+gauravaps@users.noreply.github.com>
2022-11-29 19:40:16 +0530

    Add files via upload

---
+++ python assignment -2/add comments in python.py
@@ -0,0 +1,4 @@
+# write a python script to add comments and print "learning python on screen".
+""" this is comment in pytho
+ this is not executable"""
+print("learning python")
---
+++ python assignment -2/current date and time.py
@@ -0,0 +1,4 @@
+#write a python script to display the current date. first create a variable to store it date and time,then display.
+import datetime
+now=datetime.datetime.now()
+print(now)
